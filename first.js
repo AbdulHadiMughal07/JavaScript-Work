05d7ff390db52d74f6a325662183d0123ff8bcbf
asp <asp@aptech.com>
2024-01-09 18:28:47 +0500

    Update

---
+++ first.js
@@ -0,0 +1,37 @@
+// Value Print ON Console
+
+console.log("Hello World");
+console.log("Hello World 2");
+
+// DATA TYPES 
+firstName = "HADI";
+age =16;
+x = null;
+y = undefined;
+isCnic = true;    
+
+// VAlUE PRINT WITH TYPES
+console.log(typeof(firstName));
+console.log(typeof(age));
+console.log(typeof(x));
+console.log(typeof(y));
+console.table(firstName , age , x , y , isCnic);
+
+// Tow Types Of Data Types 
+// Permitive > String > Number > Boolean > Null > undefined ;
+// Non Permitive Array > Object > Function ;
+
+var city = "Karachi";
+console.log(city);
+var city = "Lahore";
+console.log(city);
+
+let myage = 16;
+console.log(myage);
+city = 18;
+console.log(age);
+
+const pie = 3.41;
+console.log(pie);
+pie = 3.45;
+console.log(pie);

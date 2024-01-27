b235c891966c0ba38271692bffd4f551aa8d3cbe
AbdulHadiMughal07 <153829747+AbdulHadiMughal07@users.noreply.github.com>
2024-01-27 18:58:29 +0500

    done

---
+++ light.js
@@ -0,0 +1,27 @@
+let btn = document.getElementById('toggleBtn');
+let bulb = document.getElementById('bulb');
+let mq = document.getElementById('mq');
+
+btn.addEventListener('click' , togglebtn);
+    function togglebtn(){
+        if(btn.textContent.includes('On') && mq.textContent.includes('On')){
+            bulb.src = "on.png";
+            btn.textContent = "Turn Off"
+            mq.textContent = "Turn Off"
+            btn.className = "btn btn-outline-success"
+            btn.style.boxShadow = '3px 3px 4px 4px green'
+            bulb.style.boxShadow = '3px 3px 4px 4px green'
+            mq.style.color = "yellow"
+            mq.style.boxShadow = '3px 3px 4px 4px green'
+
+        }else{
+            bulb.src = "off.png";
+            btn.textContent = "Turn On"
+            mq.textContent = "Turn On"
+            btn.className = "btn btn-outline-danger"
+            btn.style.boxShadow = '3px 3px 4px 4px red'
+            bulb.style.boxShadow = '3px 3px 4px 4px red'
+            mq.style.color = "red"
+            mq.style.boxShadow = '3px 3px 4px 4px red'
+        }
+    }
